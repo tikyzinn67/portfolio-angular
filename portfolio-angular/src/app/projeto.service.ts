17cde69fce11bf60108327fa7f4eda3443722e71
tikyzinn67 <20241CTB0100058@estudantes.ifpr.edu.br>
2026-08-17 20:20:37 +0000

    Aula 18: religa ambiente e atualiza URLs da API no Codespace novo

---
+++ portfolio-angular/src/app/projeto.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Projeto {
+  id: number;
+  nome: string;
+  descricao: string;
+  tecnologias: string;
+  link_github: string;
+  ano: number;
+}
+
+@Injectable({ providedIn: 'root' })
+export class ProjetoService {
+  private http = inject(HttpClient);
+  private url = 'https://organic-disco-7vqxpxg7wjj53wq.github.dev-8000.app.github.dev/api/projetos.php';
+
+  listar(): Observable<Projeto[]> {
+    return this.http.get<Projeto[]>(this.url);
+  }
+}
---
+++ portfolio-angular/src/app/tecnologia.service.ts
@@ -0,0 +1,21 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Tecnologia {
+  id: number;
+  nome: string;
+  categoria: string;
+  descricao: string;
+  ano_criacao: number;
+}
+
+@Injectable({ providedIn: 'root' })
+export class TecnologiaService {
+  private http = inject(HttpClient);
+  private url = 'https://organic-disco-7vqxpxg7wjj53wq.github.dev-8000.app.github.dev/api/tecnologias.php';
+
+  listar(): Observable<Tecnologia[]> {
+    return this.http.get<Tecnologia[]>(this.url);
+  }
+}
